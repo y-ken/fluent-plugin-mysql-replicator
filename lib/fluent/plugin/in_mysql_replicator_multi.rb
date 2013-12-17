dd24159f4c574d4b1f82232e801fa1b04e7b46c0
y-ken <y.ken.studio@gmail.com>
2013-12-17 11:37:47 +0900

    call get_manager_connection only once

--- lib/fluent/plugin/in_mysql_replicator_multi.rb
+++ lib/fluent/plugin/in_mysql_replicator_multi.rb
@@ -30,6 +30,8 @@ module Fluent
       begin
         @threads = []
         @mutex = Mutex.new
+        @manager_db = get_manager_connection
+        @manager_db.query("SET SESSION wait_timeout=1800;")
         @threads << Thread.new {
           @hash_table_bulk_insert = []
           @hash_table_bulk_insert_last_time = Time.now
@@ -65,7 +67,6 @@ module Fluent
 
     def poll(config)
       begin
-        @manager_db = get_manager_connection
         masked_config = Hash[config.map {|k,v| (k == 'password') ? [k, v.to_s.gsub(/./, '*')] : [k,v]}]
         @mutex.synchronize {
           $log.info "mysql_replicator_multi: polling start. :config=>#{masked_config}"
